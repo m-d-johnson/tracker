947e3052c03f21c7d48e72b7f2222b7a8d906b63
Mike Johnson <mdj@mikejohnson.xyz>
2023-01-22 00:56:55 +0000

    Initial commit with test data

---
+++ data.bin.pb
@@ -0,0 +1,3 @@
+titanMike's Desktop Computer*Custom2?
+Wired Ethernet112233445566192.168.20.8"Copper*	1000 Mbps2@
+Wireless EthernetAA2233445566192.168.20.9"Radio*600 Mbps
---
+++ tracker.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+	"os"
+)
+import pb "tracker/proto"
+
+func main() {
+	titan := pb.Device{
+		Name:        "Desktop Computer",
+		Hostname:    "titan",
+		Model:       "Custom",
+		Description: "Mike's Desktop Computer",
+		Interfaces: []*pb.Device_NetworkInterface{
+			{
+				Name:      "Wired Ethernet",
+				Mac:       "112233445566",
+				Ip:        "192.168.20.8",
+				MediaType: "Copper",
+				Speed:     "1000 Mbps",
+			},
+			{
+				Name:      "Wireless Ethernet",
+				Mac:       "AA2233445566",
+				Ip:        "192.168.20.9",
+				MediaType: "Radio",
+				Speed:     "600 Mbps",
+			}},
+	}
+	fmt.Print(titan.Interfaces[0])
+	// Write the new address book back to disk.
+	out, err := proto.Marshal(&titan)
+	if err != nil {
+		log.Fatalln("Failed to encode address book:", err)
+	}
+	if err := os.WriteFile("data.bin.pb", out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+}
