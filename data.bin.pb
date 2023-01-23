de87c89eb98846c5fee6d5d79964a96e66a406e5
Mike Johnson <mdj@mikejohnson.xyz>
2023-01-23 14:25:47 -0800

    shifting stuff to linux and re-org the proto

--- data.bin.pb
+++ data.bin.pb
@@ -1,4 +1,5 @@
 
+½
 Desktop ComputertitanMike's Desktop Computer*Custom2?
 Wired Ethernet112233445566192.168.20.8"Copper*	1000 Mbps2@
-Wireless EthernetAA2233445566192.168.20.9"Radio*600 Mbps
+Wireless EthernetAA2233445566192.168.20.9"Radio*600 MbpsJhome inventory
--- tracker.go
+++ tracker.go
@@ -11,35 +11,45 @@ import (
 import pb "tracker/proto"
 
 func main() {
-	titan := pb.Device{
-		Name:        "Desktop Computer",
-		Hostname:    "titan",
-		Model:       "Custom",
-		Description: "Mike's Desktop Computer",
-		Interfaces: []*pb.Device_NetworkInterface{
+
+	titan := pb.Inventory{
+		InventoryName: "Home Inventory",
+		Devices: []*pb.Inventory_Device{
 			{
-				Name:      "Wired Ethernet",
-				Mac:       "112233445566",
-				Ip:        "192.168.20.8",
-				MediaType: "Copper",
-				Speed:     "1000 Mbps",
-			},
-			{
-				Name:      "Wireless Ethernet",
-				Mac:       "AA2233445566",
-				Ip:        "192.168.20.9",
-				MediaType: "Radio",
-				Speed:     "600 Mbps",
-			}},
-	}
-	fmt.Print(titan.Interfaces[0])
+				Name:        "Desktop Computer",
+				Hostname:    "titan",
+				Model:       "Custom",
+				Description: "Mike's Desktop Computer",
+				Interfaces: []*pb.Inventory_Device_NetworkInterface{
+					{
+						Name:      "Wired Ethernet",
+						Mac:       "112233445566",
+						Ip:        "192.168.20.8",
+						MediaType: "Copper",
+						Speed:     "1000 Mbps",
+					},
+					{
+						Name:      "Wireless Ethernet",
+						Mac:       "AA2233445566",
+						Ip:        "192.168.20.9",
+						MediaType: "Radio",
+						Speed:     "600 Mbps",
+					}},
+			}}}
+
+	fmt.Print(titan.Devices[0].Interfaces[0])
 	// Write the new address book back to disk.
-	out, err := proto.Marshal(&titan)
+	binaryProto, err := proto.Marshal(&titan)
 	textProto, _ := prototext.Marshal(&titan)
+
+	for x in titan.Devices {
+		fmt.Print(x.),
+	}
+
 	if err != nil {
 		log.Fatalln("Failed to encode address book:", err)
 	}
-	if err := os.WriteFile("data.bin.pb", out, 0644); err != nil {
+	if err := os.WriteFile("data.bin.pb", binaryProto, 0644); err != nil {
 		log.Fatalln("Failed to write address book:", err)
 	}
 	if err := os.WriteFile("data.textproto", textProto, 0644); err != nil {
